ea83cf423149bb6bb91e596e3bc3a3eef6135f3d
Jelle Roorda <jelle_roorda@hotmail.com>
2019-03-09 18:37:14 +0100

    :pencil2: Add echo statement.

--- src/includes/social_icons.php
+++ src/includes/social_icons.php
@@ -6,7 +6,7 @@
 	<?php if ('on' === et_get_option("social_divi_{$icon['name']}_enabled", 'off')) : ?>
 		<li class="et-social-icon social-divi-<?php echo $icon['name']; ?>">
 			<a href="<?php echo esc_url(et_get_option("social_divi_{$icon['name']}_url", '#')); ?>" class="icon">
-				<span><?php esc_html($icon['translated_name']); ?></span>
+				<span><?php echo esc_html($icon['translated_name']); ?></span>
 			</a>
 		</li>
 	<?php endif; ?>
