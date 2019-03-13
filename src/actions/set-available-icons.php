526451cda4d9e4935e64fe5775fda580706431ee
Jelle Roorda <jelle_roorda@hotmail.com>
2019-03-13 22:18:50 +0100

    :config: Make more icons available.

--- src/actions/set-available-icons.php
+++ src/actions/set-available-icons.php
@@ -10,11 +10,79 @@ global $social_divi_available_icons;
  */
 $social_divi_available_icons = [
     [
+        'name' => 'facebook',
+        'translated_name' =>  _x('Facebook', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'twitter',
+        'translated_name' =>  _x('Twitter', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'linkedin',
+        'translated_name' =>  _x('LinkedIn', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'youtube',
+        'translated_name' =>  _x('YouTube', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'vimeo',
+        'translated_name' =>  _x('Vimeo', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'medium',
+        'translated_name' =>  _x('Medium', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'tumblr',
+        'translated_name' =>  _x('Tumblr', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'pinterest',
+        'translated_name' =>  _x('Pinterest', 'Icon name in the interface', 'social-divi'),
+    ],    
+    [
+        'name' => 'instagram',
+        'translated_name' =>  _x('Instagram', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'snapchat',
+        'translated_name' =>  _x('Snapchat', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'periscope',
+        'translated_name' =>  _x('Periscope', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
         'name' => 'soundcloud',
         'translated_name' =>  _x('SoundCloud', 'Icon name in the interface', 'social-divi'),
     ],
     [
-        'name' => 'facebook',
-        'translated_name' =>  _x('Facebook', 'Icon name in the interface', 'social-divi'),
+        'name' => 'xing',
+        'translated_name' =>  _x('Xing', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'behance',
+        'translated_name' =>  _x('Behance', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'bloglovin',
+        'translated_name' =>  _x('BlogLovin', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'dribbble',
+        'translated_name' =>  _x('Dribbble', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'flickr',
+        'translated_name' =>  _x('Flickr', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'github',
+        'translated_name' =>  _x('Github', 'Icon name in the interface', 'social-divi'),
+    ],
+    [
+        'name' => 'rss',
+        'translated_name' =>  _x('RSS', 'Icon name in the interface', 'social-divi'),
     ],
 ];
