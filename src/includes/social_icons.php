d865f1eec93ed73ad142bfa7714ede402177f4ac
Jelle Roorda <jelle_roorda@hotmail.com>
2019-03-16 18:23:36 +0100

    :lock: Increase security by escaping attributes on icons.

--- src/includes/social_icons.php
+++ src/includes/social_icons.php
@@ -6,7 +6,7 @@
 	<?php if ('on' === et_get_option("social_divi_{$icon['name']}_enabled", 'off')) : ?>
 		<li class="et-social-icon social-divi-<?php echo $icon['name']; ?>">
 			<a href="<?php echo esc_url(et_get_option("social_divi_{$icon['name']}_url", '#')); ?>" title="<?php echo esc_html($icon['translated_name']); ?>" class="icon">
-				<i class="<?php echo $icon['fa_primary']; ?> fa-<?php echo $icon['name']; ?>"></i>
+				<i class="<?php echo esc_attr($icon['fa_primary']); ?> fa-<?php echo esc_attr($icon['name']); ?>"></i>
 			</a>
 		</li>
 	<?php endif; ?>
