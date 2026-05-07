f8438af06b3d799fb9cd71ef9bb3edd1760dabb6
Jelle Roorda <info@roordaict.nl>
2026-05-07 17:09:07 +0200

    Fix formatting.

--- src/actions/set-available-icons.php
+++ src/actions/set-available-icons.php
@@ -14,145 +14,145 @@ add_action('init', function () {
     global $social_divi_available_icons;
 
     $social_divi_available_icons = [
-    [
-        'name' => 'whatsapp',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('WhatsApp', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'facebook-f',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Facebook', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'twitter',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Twitter', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'x-twitter',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('X (formerly Twitter)', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'bluesky',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Bluesky', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'linkedin-in',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('LinkedIn', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'youtube',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('YouTube', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'vimeo-v',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Vimeo', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'medium-m',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Medium', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'tumblr',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Tumblr', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'pinterest-p',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Pinterest', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'instagram',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Instagram', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'snapchat',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Snapchat', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'telegram',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Telegram', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'tiktok',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('TikTok', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'twitch',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Twitch', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'periscope',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Periscope', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'soundcloud',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('SoundCloud', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'spotify',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Spotify', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'apple',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Apple', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'xing',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Xing', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'behance',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Behance', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'dribbble',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Dribbble', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'flickr',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Flickr', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'github',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('GitHub', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'discord',
-        'fa_primary' => 'fab',
-        'translated_name' =>  _x('Discord', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'houzz',
-        'fa_primary' => 'fa',
-        'translated_name' =>  _x('Houzz', 'Icon name in the interface', 'social-divi'),
-    ],
-    [
-        'name' => 'rss',
-        'fa_primary' => 'fas',
-        'translated_name' =>  _x('RSS', 'Icon name in the interface', 'social-divi'),
-    ],
-];
+        [
+            'name' => 'whatsapp',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('WhatsApp', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'facebook-f',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Facebook', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'twitter',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Twitter', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'x-twitter',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('X (formerly Twitter)', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'bluesky',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Bluesky', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'linkedin-in',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('LinkedIn', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'youtube',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('YouTube', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'vimeo-v',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Vimeo', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'medium-m',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Medium', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'tumblr',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Tumblr', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'pinterest-p',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Pinterest', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'instagram',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Instagram', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'snapchat',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Snapchat', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'telegram',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Telegram', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'tiktok',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('TikTok', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'twitch',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Twitch', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'periscope',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Periscope', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'soundcloud',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('SoundCloud', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'spotify',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Spotify', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'apple',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Apple', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'xing',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Xing', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'behance',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Behance', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'dribbble',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Dribbble', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'flickr',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Flickr', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'github',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('GitHub', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'discord',
+            'fa_primary' => 'fab',
+            'translated_name' => _x('Discord', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'houzz',
+            'fa_primary' => 'fa',
+            'translated_name' => _x('Houzz', 'Icon name in the interface', 'social-divi'),
+        ],
+        [
+            'name' => 'rss',
+            'fa_primary' => 'fas',
+            'translated_name' => _x('RSS', 'Icon name in the interface', 'social-divi'),
+        ],
+    ];
 });
